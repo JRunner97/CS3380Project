9fe7b9f3a7a1527502133b55ed13eac2fd97bf14
Joshua Thompson <jthompson706.jt@gmail.com>
2018-10-10 13:55:44 -0500

    process login form data

--- login.php
+++ login.php
@@ -3,22 +3,12 @@
     // define variables and set to empty values
 //    $name = $email = $gender = $comment = $website = "";
 //
-//    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//      $username = $_POST["user"];
-//    }
-//
-//    $txt1 = "Learn PHP";
-//
-//    echo "<h2>" . $txt1 . "</h2>";
-//
-//    echo "<h1>" . $username . "</h1>";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = htmlspecialchars($_POST['user']);
+        $password  = htmlspecialchars($_POST['password']);
 
-     // Check if the form is submitted
-    if ( isset( $_POST['submit'] ) ) {
-
-    // retrieve the form data by using the element's name attributes value as key
-
-    echo '<h2>form data retrieved by using the $_REQUEST variable<h2/>';
+        echo  $username, ' ', $password, implode("",$_REQUEST);
     }
+  
 
 ?>
