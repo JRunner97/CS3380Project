09555c1b72e1a1586a121fc2d1febcbff32bbd12
Ben Bowman (In Class) <bkbtk4@mail.missouri.edu>
2018-11-19 00:46:06 -0600

    Changed functionality for groceryList.php

--- GroceryList/groceryList.php
+++ GroceryList/groceryList.php
@@ -5,6 +5,9 @@ session_start();
 
 <?php
 
+    header("Content-Type: application/json; Applications/XAMPP/htdocs/CS3380Project/GroceryList/groceryList.html");
+    $obj = json_decode($_POST["x"], false);
+
     $servername = "ec2-18-218-134-37.us-east-2.compute.amazonaws.com";
     $dbUsername = "ProjectUser";
     $dbPassword = "12345";
@@ -15,26 +18,16 @@ session_start();
         
         //Getting the current users ID
         $userID = $_SESSION(currentUser);
+        //$userID = 2; //Used for testing
         
-        //Getting any recipes that the current user favorited
-        $favoriteRecipes = $conn->prepare("SELECT recipeID FROM favorite WHERE userID = '$userID'");
-        $favoriteRecipes->execute();
+        //Getting ingredients form favorited recipes
+        $ingredients = $conn->prepare("SELECT R.ingredients FROM favorite F, recipes R WHERE F.userID = '$userID' AND F.recipeID = R.recipeID");
+        $ingredients->bind_param("s", $obj->table);
+        $ingredients->execute();
+        $result = $ingredients->get_result();
+        $outp = $result->fetch_all();
         
-        //Checking if there are any favorited recipes
-        if($favoriteRecipes->rowCount() != 0){
-            
-            //Getting the ingredients of recipes that the user has favorited
-            $favoriteIngredients = $conn->prepare("SELECT ingredients FROM recipes WHERE recipeID IN '$favoriteRecipes'");
-            $favoriteIngredients->execute();
-            
-        }
-        else {
-            
-            //Not sure if this is how to display back on the page
-            echo "There are no items on your grocery list.";
-            
-            exit();
-        }
+        echo json_encode($outp);
         
     }
     catch(PDOException $e){
